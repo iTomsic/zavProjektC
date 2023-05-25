46039207c7d6caca1355dbcb1bc180c9cb6b49e5
iTomsic <79262118+iTomsic@users.noreply.github.com>
2023-05-25 13:24:27 +0200

    Update header.h

--- header.h
+++ header.h
@@ -5,15 +5,26 @@
 //3
 typedef struct komp {
 	int cijena;
-	char marka[25];
-	char imeKomponente[15];
+	char marka[50];
+	char imeKomponente[50];
 	int id;
 }KOMPONENTA;
 
 int menu();
 void kreiranjeDat();
+int menuSearch();
+int menuSort();
 void dodavanjeKomponente();
 void* ucitavanjeKomponente();
+void ispis(KOMPONENTA*);
+void* searchMarka(KOMPONENTA*);
+void* searchImeProizvoda(KOMPONENTA*);
+void* searchCijena(KOMPONENTA*);
+void subZaSortSil(KOMPONENTA*, KOMPONENTA*);
+void subZaSortUzl(KOMPONENTA*, KOMPONENTA*);
+void selectionSortCijenaUzl(KOMPONENTA*);
+void selectionSortCijenaSil(KOMPONENTA*);
+void brisanjeKomponente(KOMPONENTA*);
 void izlaz(KOMPONENTA*);
 
 #endif //HEADER_H
