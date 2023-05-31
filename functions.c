3ec3514ea97a83d11fd53d02e587563b852d550d
iTomsic <79262118+iTomsic@users.noreply.github.com>
2023-05-31 23:13:37 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -9,23 +9,26 @@
 //19 u funckijama koristi se perror (implementirano u funk)
 
 
-static int brojProizvoda = 0, i, j, br = 0; //5
+static int brojKomponenti = 0, i, j, br = 0; //5
 
 //8
-int menu() 
+int menu()
 {
 	int izbor = 0;
-	static KOMPONENTA* polje = NULL; 
+	static KOMPONENTA* polje = NULL;
 
+	printf("\nIzaberite opciju:\n\n");
+	printf("  1.	Dodavanje komponente\n");
 	printf("----------------------------------------------------------------------------------\n");
-	printf("Izaberite opciju:\n\n");
-	printf("1.	Dodaj proizvod\n");
-	printf("2.	Uredi postojeci proizvod\n");
-	printf("3.	Ispis proizvoda\n");
-	printf("4.	Trazenje\n");
-	printf("5.	Sortiraj proizvode po cijeni\n");
-	printf("6.	Brisanje proizvoda\n");
-	printf("7.	Izlaz iz programa\n");
+	printf("  2.	Ispis komponente\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("  3.	Trazenje po kategoriji\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("  4.	Sortiranje komponenti po cijeni\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("  5.	Brisanje komponenti\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("  6.	Izlaz iz programa\n");
 	printf("----------------------------------------------------------------------------------\n");
 
 	scanf("%d", &izbor);
@@ -34,125 +37,163 @@ int menu()
 
 	switch (izbor) {
 	case 1:
+
 		ucitavanjeKomponente();
-		if (brojProizvoda == 0) {
+		if (brojKomponenti == 0) {
 			kreiranjeDat();
 		}
 		dodavanjeKomponente();
-		printf("Odabrano 1. Dodaj proizvod\n");
 		break;
+
 	case 2:
-		
-		printf("Odabrano 2. Uredi postojeci proizvod \n");
-		break;
-	case 3:
+
 		if (polje != NULL) {
 			free(polje);		//15
 			polje = NULL;		//15
 		}
 		polje = (KOMPONENTA*)ucitavanjeKomponente();
 		ispis(polje);
-		printf("Odabrano 3. Ispis proizvoda \n");
 		break;
-	case 4:
+
+	case 3:
+
 		while (izbor != 997) {
-			 izbor = menuSearch();
+			izbor = menuSearch();
 		}
-		printf("Odabrano 4. Trazenje \n");
 		break;
-	case 5:
+
+	case 4:
+
 		while (izbor != 998)
 			izbor = menuSort();
-		printf("Odabrano 5. Sortiraj proizvode po cijeni \n");
 		break;
-	case 6:
+
+	case 5:
+
 		if (polje == NULL)
-			printf("Nema komponenti u skladistu.");
+			printf("\nNema komponenti u skladistu.\n");
 		else
 			brisanjeKomponente(polje);
-		printf("Odabrano 6. Brisanje proizvoda \n");
 		break;
-	case 7:
+
+	case 6:
+
 		izlaz(polje);
 		return 999;
 		break;
+
 	default:
-		printf("\nOdabrali ste pogresan broj!\nMolim probajte ponovno.\n");
+
+		printf("\nOdabrali ste pogresan broj!\nMolim pokusajte ponovno.\n");
+
 	}
 
 	return izbor;
 }
 
 //8,21
-int menuSearch() 
+int menuSearch()
 {
 	int izbor = 0;
-	KOMPONENTA* polje = NULL;			
+	KOMPONENTA* polje = NULL;
 	polje = (KOMPONENTA*)ucitavanjeKomponente();	//13
-	printf("Pretrazi po:\n");
-	printf("\t 1. Imenu tvrtke\n");
-	printf("\t 2. Imenu proizvoda\n");
-	printf("\t 3. Cijeni\n");
-	printf("\t 4. Natrag\n");
+
+	printf("\nPretrazivanje po:\n\n");
+	printf("\t 1. ID-u\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("\t 2. Imenu tvrtke\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("\t 3. Imenu proizvoda\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("\t 4. Cijeni\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("\t 5. Povratak\n");
+	printf("----------------------------------------------------------------------------------\n");
 
 	scanf("%d", &izbor);
 	system("cls");
 
 	switch (izbor) {
 	case 1:
+		
+		searchID(polje);
+		break;
+
+	case 2:
+		
 		searchMarka(polje);
 		break;
-	case 2:
-		searchImeProizvoda(polje);
-		break;
+
 	case 3:
+		
+		searchImeKomponente(polje);
+		break;
+
+	case 4:
+		
 		searchCijena(polje);
 		break;
-	case 4:
+
+	case 5:
+		
 		return 997;
-	default: printf("Odabrali ste pogresan broj!\nMolim probajte ponovno.\n");
+
+	default: printf("Odabrali ste pogresan broj!\nMolim pokusajte ponovno.\n");
+
 	}
 	return izbor;
 }
 
 //8,20
-int menuSort() 
+int menuSort()
 {
 	int izbor = 0;
 	KOMPONENTA* polje = NULL;						//13
 	polje = (KOMPONENTA*)ucitavanjeKomponente();
-	printf("Sortiranje po:\n");
+
+	printf("\nSortiranje po:\n\n");
+	printf("----------------------------------------------------------------------------------\n");
 	printf("\t 1. Cijeni uzlazno\n");
+	printf("----------------------------------------------------------------------------------\n");
 	printf("\t 2. Cijeni silazno\n");
-	printf("\t 3. Natrag\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("\t 3. Povratak\n");
+	printf("----------------------------------------------------------------------------------\n");
 
 	scanf("%d", &izbor);
 	system("cls");
 
 	switch (izbor) {
 	case 1:
+
 		selectionSortCijenaUzl(polje);
 		break;
+
 	case 2:
+
 		selectionSortCijenaSil(polje);
 		break;
+
 	case 3:
+
 		return 998;
+
 	default: printf("Odabrali ste pogresan broj!\nMolim pokusajte ponovno.\n");
+
 	}
 	return izbor;
 }
 
 //16
-void kreiranjeDat() 
+void kreiranjeDat()
 {
-	FILE* fp = NULL;						
+	FILE* fp = NULL;
 	fp = fopen("komponente.bin", "wb");
 	if (fp == NULL) {
 		perror("Kreiranje"); 				//19
 	}
 
-	fwrite(&brojProizvoda, sizeof(int), 1, fp);		//17
+	fwrite(&brojKomponenti, sizeof(int), 1, fp);		//17
 
 	fclose(fp);
 }
@@ -160,59 +201,56 @@ void kreiranjeDat()
 //16,17
 void dodavanjeKomponente()
 {
-	FILE* fp = NULL;						
+	FILE* fp = NULL;
 	fp = fopen("komponente.bin", "rb+");
 	if (fp == NULL)
 		perror("Dodavanje");				//19
 
-	fread(&brojProizvoda, sizeof(int), 1, fp);
-	printf("Trenutni broj proizvoda: %d\n", brojProizvoda);
+	fread(&brojKomponenti, sizeof(int), 1, fp);
+	printf("Trenutni broj proizvoda: %d\n", brojKomponenti);
 
-	KOMPONENTA proizvod;
-	proizvod.id = brojProizvoda;
+	KOMPONENTA komponenta;
+	komponenta.id = brojKomponenti;
 	br++;
 
 	getchar();
 	printf("Unesite ime tvrtke: ");
-	scanf("%24[^\n]", proizvod.marka);
-
+	scanf("%24[^\n]", komponenta.marka);
 	getchar();
 	printf("Unesite ime proizvoda: ");
-	scanf("%9[^\n]", proizvod.imeKomponente);
-
+	scanf("%9[^\n]", komponenta.imeKomponente);
 	getchar();
 	printf("Unesite cijenu proizvoda: ");
-	scanf("%d", &proizvod.cijena);
-
+	scanf("%d", &komponenta.cijena);
 	getchar();
 
-	fseek(fp, sizeof(KOMPONENTA) * brojProizvoda, SEEK_CUR);	//17
-	fwrite(&proizvod, sizeof(KOMPONENTA), 1, fp);
+	fseek(fp, sizeof(KOMPONENTA) * brojKomponenti, SEEK_CUR);	//17
+	fwrite(&komponenta, sizeof(KOMPONENTA), 1, fp);
 	rewind(fp);							//17
-	brojProizvoda++;
-	fwrite(&brojProizvoda, sizeof(int), 1, fp);			//17
+	brojKomponenti++;
+	fwrite(&brojKomponenti, sizeof(int), 1, fp);			//17
 
 	fclose(fp);
 }
 
 //13,14,16
-void* ucitavanjeKomponente() 
+void* ucitavanjeKomponente()
 {
-	FILE* fp = fopen("komponente.bin", "rb");							
+	FILE* fp = fopen("komponente.bin", "rb");
 	if (fp == NULL) {
 		printf("Niste unijeli niti jedan proizvod.\n");
 		return NULL;
 	}
 
-	fread(&brojProizvoda, sizeof(int), 1, fp);
-	KOMPONENTA* polje = NULL;																
-	polje = (KOMPONENTA*)calloc(brojProizvoda, sizeof(KOMPONENTA));	//13
+	fread(&brojKomponenti, sizeof(int), 1, fp);
+	KOMPONENTA* polje = NULL;
+	polje = (KOMPONENTA*)calloc(brojKomponenti, sizeof(KOMPONENTA));	//13
 	if (polje == NULL) {
 		perror("Zauzimanje memorije");			//19
 		return NULL;
 	}
 
-	fread(polje, sizeof(KOMPONENTA), brojProizvoda, fp);
+	fread(polje, sizeof(KOMPONENTA), brojKomponenti, fp);
 
 	fclose(fp);
 
@@ -221,15 +259,32 @@ void* ucitavanjeKomponente()
 
 void ispis(KOMPONENTA* polje)
 {
-	for (i = 0; i < brojProizvoda; i++) {
-		/*printf("Ime tvrtke:%s  Ime proizvoda:%s  Cijena: %d\n", (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);*/
-
+	for (i = 0; i < brojKomponenti; i++) {
 		printf("\nID: %d  Ime tvrtke:%s  Ime proizvoda:%s  Cijena: %d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
 	}
 }
 
+void* searchID(KOMPONENTA* polje)
+{
+	int trazenaID, br = 0;
+
+	printf("Unesite broj ID-a kojeg trazite:\n");
+	scanf("%d", &trazenaID);
+
+	for (i = 0; i < brojKomponenti; i++) {
+		if (trazenaID == (polje + i)->id) {
+
+			printf("\nID: %d  Ime tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
+			br++;
+		}
+	}
+	if (br == 0)
+		printf("\nTrazen ID nije pronadjen.\n");
+	return NULL;
+}
+
 //21
-void* searchMarka(KOMPONENTA* polje) 
+void* searchMarka(KOMPONENTA* polje)
 {
 	char trazenaMarka[50];
 	int br = 0;
@@ -238,11 +293,10 @@ void* searchMarka(KOMPONENTA* polje)
 	getchar();
 	scanf("%49[^\n]", trazenaMarka);
 
-	for (i = 0; i < brojProizvoda; i++) {
+	for (i = 0; i < brojKomponenti; i++) {
 		if (!strcmp(trazenaMarka, (polje + i)->marka)) {
-			
-			printf("\nIme tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
 
+			printf("\nID: %d  Ime tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
 			br++;
 		}
 	}
@@ -252,7 +306,7 @@ void* searchMarka(KOMPONENTA* polje)
 }
 
 //21
-void* searchImeProizvoda(KOMPONENTA* polje) {
+void* searchImeKomponente(KOMPONENTA* polje) {
 	char trazenProizvod[50];
 	int br = 0;
 
@@ -260,13 +314,10 @@ void* searchImeProizvoda(KOMPONENTA* polje) {
 	getchar();
 	scanf("%49[^\n]", trazenProizvod);
 
-	for (i = 0; i < brojProizvoda; i++) {
+	for (i = 0; i < brojKomponenti; i++) {
 		if (!strcmp(trazenProizvod, (polje + i)->imeKomponente)) {
-			
-			printf("\nIme tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
 
-			//ispis(trazenProizvod);
-
+			printf("\nID: %d  Ime tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
 			br++;
 		}
 	}
@@ -282,10 +333,10 @@ void* searchCijena(KOMPONENTA* polje) {
 	printf("Unesite broj cijene koju trazite:\n");
 	scanf("%d", &trazenaCijena);
 
-	for (i = 0; i < brojProizvoda; i++) {
+	for (i = 0; i < brojKomponenti; i++) {
 		if (trazenaCijena == (polje + i)->cijena) {
-			
-			printf("\nIme tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
+
+			printf("\nID: %d  Ime tvrtke : % s  Ime proizvoda : % s  Cijena : % d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
 
 			br++;
 		}
@@ -296,7 +347,7 @@ void* searchCijena(KOMPONENTA* polje) {
 }
 
 //20
-void subZaSortSil(KOMPONENTA* veci, KOMPONENTA* manji) 
+void subZaSortSil(KOMPONENTA* veci, KOMPONENTA* manji)
 {
 	KOMPONENTA temp = { 0 };
 	temp = *veci;
@@ -305,12 +356,40 @@ void subZaSortSil(KOMPONENTA* veci, KOMPONENTA* manji)
 }
 
 //20
-void subZaSortUzl(KOMPONENTA* manji, KOMPONENTA* veci) 
+void subZaSortUzl(KOMPONENTA* manji, KOMPONENTA* veci)
 {
 	KOMPONENTA temp = { 0 };
 	temp = *manji;
 	*manji = *veci;
 	*veci = temp;
+}
+
+void selectionSortCijenaUzl(KOMPONENTA* polje) {
+	for (int j = 0; j < brojKomponenti; j++) {
+		for (int i = 0; i < brojKomponenti - 1; i++) {
+			if (polje[i].cijena > polje[i + 1].cijena) {
+				subZaSortUzl((polje + i), (polje + i + 1));
+			}
+		}
+	}
+	for (i = 0; i < brojKomponenti; i++) {
+		printf("\nID: %d Ime tvrtke:%s  Ime proizvoda:%s  Cijena: %d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
+	}
+	printf("\n");
+}
+
+void selectionSortCijenaSil(KOMPONENTA* polje) {
+	for (int j = 0; j < brojKomponenti; j++) {
+		for (int i = 0; i < brojKomponenti - 1; i++) {
+			if (polje[i].cijena < polje[i + 1].cijena) {
+				subZaSortSil((polje + i), (polje + i + 1));
+			}
+		}
+	}
+	for (i = 0; i < brojKomponenti; i++) {
+		printf("\nID: %d Ime tvrtke:%s  Ime proizvoda:%s  Cijena: %d\n", (polje + i)->id, (polje + i)->marka, (polje + i)->imeKomponente, (polje + i)->cijena);
+	}
+	printf("\n");
 }
 
 //16
@@ -343,22 +422,22 @@ void brisanjeKomponente(KOMPONENTA* polje)
 
 }
 
-void izlaz(KOMPONENTA* polje) 
+void izlaz(KOMPONENTA* polje)
 {
-	printf("\nZelite li pri izlasku programa izbrisati datoteku ili zadrzati?\n");
-	printf("Ako zelite izbrisati datoteku napisite 'OBRISI', ako zelite zadrzati upisite bilo sto\n\n");
+	printf("Zelite li pri izlasku izbrisati datoteku ili je zadrzati?\n");
+	printf("Ukoliko zelite izbrisati datoteku napisite 'OBRISI'\n\n");
 
 	char uvjet[7] = { '\0' }; //12
 	scanf("%6s", uvjet);
 	if (!strcmp("OBRISI", uvjet)) {
-		remove("komponente.bin") == 0 ? printf("Izbrisali ste datoteku komponente.bin.\n") : printf("Datoteka neuspjesno izbrisani ili ona ne postoji.\n");	//18
-		printf("\nIzlaz iz programa.\n");
+		remove("komponente.bin") == 0 ? printf("\nDatoteka komponente.bin uspjesno izbrisana.\n") : printf("\nDatoteka neuspjesno izbrisana ili ne postoji.\n");	//18
+		printf("\n\nIzlaz iz programa.\n");
 		free(polje);
 	}
-	else 
+	else
 	{
 		free(polje);
-		printf("\nZadrzali ste datoteku.\nIzlaz iz programa\n");
+		printf("\nZadrzali ste datoteku.\n\nIzlaz iz programa.\n");
 	}
-		
+
 }
